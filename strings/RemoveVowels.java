0894f9a5f93e43f178ed68389bf908a407048d21
Vedant <vedantpardeshi26@gmail.com>
2025-09-16 19:16:05 +0530

    Solved remove vowels problem

---
+++ strings/RemoveVowels.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class RemoveVowels {
+    public static String removeVowels(String str){
+        // Using hashset because efficiency O(1)
+        Set<Character> vowels = new HashSet<>(Arrays.asList(
+            'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+        ));
+
+        StringBuilder newStr = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++) {
+            if(!vowels.contains(str.charAt(i))) newStr.append(str.charAt(i));
+        }
+
+        return newStr.toString();
+    }
+    public static void main(String[] args) {
+        String str = "Hello World";
+
+        System.out.println("Original String : " + str);
+        System.out.println("String without vowels : " + removeVowels(str));
+        
+    }
+}
