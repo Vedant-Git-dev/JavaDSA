1775f8988ba308bd9ed6ca5ada8a8603adf35df1
Vedant <vedantpardeshi26@gmail.com>
2025-09-22 13:54:42 +0530

    Improved the solution of CountFrequency.java using HashMap

--- strings/CountFrequency.java
+++ strings/CountFrequency.java
@@ -1,18 +1,22 @@
-public class CountFrequency {
-    public static int countFrequency(String str, char ch){
-        int count = 0;
+import java.util.*;
 
-        for (int i = 0; i < str.length(); i++) {
-            if(ch == str.charAt(i)){
-                count++;
-            }
+public class CountFrequency {
+    public static void countFrequency(String str){
+        HashMap <Character, Integer> frequency = new HashMap<>();
+
+        for(char ch : str.toCharArray()) {
+            // Checks the frequency of character or set it zero by default and tracks it  
+            frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
         }
 
-        return count;
+        System.out.println("Frequencies : ");
+        for(Map.Entry<Character, Integer> entry : frequency.entrySet()){
+            System.out.println(entry.getKey() + " --> " + entry.getValue());
+        }
     }
     public static void main(String args[]){
-        String str = "Hello, I am Vedant, curently learning DSA";
+        String str = "Hello World";
         
-        System.out.println("Frequency of l : " + countFrequency(str, 'l'));
+        countFrequency(str);
     }
 }
